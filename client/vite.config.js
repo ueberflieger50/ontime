2a73cfd2db487f1bdcdc6889875c0e4a0f8b8783
cv <34649812+cpvalente@users.noreply.github.com>
2022-12-07 08:57:15 +0100

    improvement: compile with legacy compatibility to chrome 71

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,18 @@
+import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgrPlugin from 'vite-plugin-svgr';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+    legacy({
+      targets: ['chrome 71'],
+    }),
+  ],
   server: {
     port: 3000,
   },
@@ -14,6 +22,6 @@ export default defineConfig({
     environment: 'jsdom',
   },
   build: {
-    outDir: './build'
+    outDir: './build',
   }
 });
